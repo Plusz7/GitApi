8d842300c09b61782a4f526ef51d189aa6a18069
Plusz7 <darek.pluszczynski@gmail.com>
2023-09-01 16:47:48 +0200

    Added exception handling for failed calls in GitHubRepository

--- src/main/kotlin/com/practice/gitapi/controller/GitHubController.kt
+++ src/main/kotlin/com/practice/gitapi/controller/GitHubController.kt
@@ -1,9 +1,8 @@
 package com.practice.gitapi.controller
 
-import com.practice.gitapi.model.dto.BranchDto
-import com.practice.gitapi.model.dto.UserRepositoryDto
-import com.practice.gitapi.model.response.UserRepositoryResponse
 import com.practice.gitapi.service.GetGitHubService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +15,18 @@ class GitHubController(
 ) {
 
     @GetMapping("/user/{username}/repos")
-    fun getUserRepositories(@PathVariable username: String): List<UserRepositoryResponse> {
-        return getGitHubService.getRepositoryFromUser(username)
+    fun getUserRepositories(@PathVariable username: String): ResponseEntity<Any> {
+        val repositories = getGitHubService.getRepositoryFromUser(username)
+        return if (repositories.isNotEmpty()) {
+            ResponseEntity.ok(repositories)
+        } else {
+            ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(
+                    mapOf(
+                        "status" to HttpStatus.NOT_FOUND.value(),
+                        "message" to "User not found."
+                    )
+                )
+        }
     }
 }
