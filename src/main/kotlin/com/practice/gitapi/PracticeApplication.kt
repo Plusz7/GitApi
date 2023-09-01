c3ec896cb0644aa1e967974318377a960d8c364d
Plusz7 <darek.pluszczynski@gmail.com>
2023-09-01 12:53:07 +0200

    Initial commit

---
+++ src/main/kotlin/com/practice/gitapi/PracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.practice.gitapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<PracticeApplication>(*args)
+}
