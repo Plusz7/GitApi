19145b88319574e2d64c1ff8dc1ff5acbcb059ca
Plusz7 <darek.pluszczynski@gmail.com>
2023-09-07 15:37:22 +0200

    implemented integration test, changed webClient config, autowired webClientBuilder onto GitHubRepository

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.0")
     testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+    testImplementation("org.assertj:assertj-core:3.21.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
 }
 
---
+++ src/test/kotlin/com/practice/gitapi/integration/repository/GitHubRepositoryIntegrationTest.kt
@@ -0,0 +1,84 @@
+package com.practice.gitapi.integration.repository
+
+import com.practice.gitapi.config.TestConfig
+import com.practice.gitapi.repository.GitHubRepository
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.web.reactive.function.client.WebClient
+import org.assertj.core.api.Assertions.*
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [TestConfig::class])
+class GitHubRepositoryIntegrationTest {
+
+    @Autowired
+    private lateinit var gitHubRepository: GitHubRepository
+
+    @Autowired
+    private lateinit var mockWebServer: MockWebServer
+
+    @Autowired
+    private lateinit var webClient: WebClient
+
+    private final val GET_BODY_REPONSE = """
+        [
+            {
+                "id": 1,
+                "node_id": "node1",
+                "name": "Repo1",
+                "full_name": "User1/Repo1",
+                "owner": {
+                    "login": "User1",
+                    "url": "https://github.com/User1"
+                },
+                "private": false,
+                "html_url": "https://github.com/User1/Repo1"
+            },
+            {
+                "id": 2,
+                "node_id": "node2",
+                "name": "Repo2",
+                "full_name": "User1/Repo2",
+                "owner": {
+                    "login": "User1",
+                    "url": "https://github.com/User1"
+                },
+                "private": true,
+                "html_url": "https://github.com/User1/Repo2"
+            }
+        ]
+    """
+
+    @BeforeEach
+    fun setup() {
+        mockWebServer.enqueue(MockResponse().setBody(GET_BODY_REPONSE).setResponseCode(200))
+    }
+
+    @AfterEach
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun fetchUserRepos_ShouldReturnCorrectRepos() {
+        val repos = gitHubRepository.getRepositoryFromUser("User1")
+
+        assertThat(repos).isNotNull()
+        assertThat(repos).hasSize(1)
+    }
+
+    @Test
+    fun fetchUserRepose_NoUserFoundShouldReturnEmptyList() {
+        val repos = gitHubRepository.getRepositoryFromUser("User3")
+
+        assertThat(repos).isNotNull()
+        assertThat(repos).isEmpty()
+    }
+}
